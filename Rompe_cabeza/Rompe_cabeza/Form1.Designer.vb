aa8c03e7325cda46ab558830d26e464cd0cced64
juanbog <juancho18_81@hotmail.com>
2015-04-24 00:43:23 -0500

    Juan martinez
    Segunda version

---
+++ Rompe_cabeza/Rompe_cabeza/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.Black
+        Me.Button10.BackgroundImage = Global.Rompe_cabeza.My.Resources.Resources.foto
+        Me.Button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button10.Location = New System.Drawing.Point(443, 12)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(400, 344)
+        Me.Button10.TabIndex = 9
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.White
+        Me.PictureBox2.Location = New System.Drawing.Point(152, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.White
+        Me.PictureBox3.Location = New System.Drawing.Point(292, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox3.TabIndex = 12
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.White
+        Me.PictureBox4.Location = New System.Drawing.Point(12, 132)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox4.TabIndex = 13
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.White
+        Me.PictureBox5.Location = New System.Drawing.Point(152, 132)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox5.TabIndex = 14
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.White
+        Me.PictureBox6.Location = New System.Drawing.Point(292, 132)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox6.TabIndex = 15
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.White
+        Me.PictureBox7.Location = New System.Drawing.Point(12, 252)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox7.TabIndex = 16
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.White
+        Me.PictureBox8.Location = New System.Drawing.Point(152, 252)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox8.TabIndex = 17
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.Black
+        Me.PictureBox9.Location = New System.Drawing.Point(292, 252)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(134, 114)
+        Me.PictureBox9.TabIndex = 18
+        Me.PictureBox9.TabStop = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "Fot.jpg")
+        Me.ImageList1.Images.SetKeyName(1, "foto1_part1x1.jpg")
+        Me.ImageList1.Images.SetKeyName(2, "foto1_part1x2.jpg")
+        Me.ImageList1.Images.SetKeyName(3, "foto1_part1x3.jpg")
+        Me.ImageList1.Images.SetKeyName(4, "foto1_part2x1.jpg")
+        Me.ImageList1.Images.SetKeyName(5, "foto1_part2x2.jpg")
+        Me.ImageList1.Images.SetKeyName(6, "foto1_part2x3.jpg")
+        Me.ImageList1.Images.SetKeyName(7, "foto1_part3x1.jpg")
+        Me.ImageList1.Images.SetKeyName(8, "foto1_part3x2.jpg")
+        Me.ImageList1.Images.SetKeyName(9, "foto1_part3x3.jpg")
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(326, 409)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 40)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "INICIAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(892, 486)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button10)
+        Me.Name = "Form1"
+        Me.Text = "Rompe_cabeza"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Rompe_cabeza/Rompe_cabeza/Form1.vb
@@ -0,0 +1,238 @@
+﻿'contiene la clase Form1, funcion de inicio de juego, funciones que permite realizar comparaciones  
+
+Public Class Form1
+    'formulario que contiene 9 cuadros de imagen, los cuales se desplazan segun movimientos realizados por los 
+    'usuarios y con condiciones especificas las cuales estan dadas en las funciones de cada duadro de imagen 
+    'para asi poder organizar la imagen como se tiene el mostrario.
+
+    Private Sub InicioJuego_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'funcion que se ejecuta cuando se da clic en el bonton iniciar, organiza las imagenes en los cuadros de imagenes y organiza
+        'los colores de fondo de las los cuadros de imagenes para poder comparar y desplazar.
+        PictureBox1.Image = ImageList1.Images(6)
+        PictureBox2.Image = ImageList1.Images(1)
+        PictureBox3.Image = ImageList1.Images(5)
+        PictureBox4.Image = ImageList1.Images(2)
+        PictureBox6.Image = ImageList1.Images(3)
+        PictureBox5.Image = ImageList1.Images(7)
+        PictureBox7.Image = ImageList1.Images(8)
+        PictureBox8.Image = ImageList1.Images(4)
+        PictureBox9.Image = ImageList1.Images(0)
+
+        PictureBox1.BackColor = Color.White
+        PictureBox2.BackColor = Color.White
+        PictureBox3.BackColor = Color.White
+        PictureBox4.BackColor = Color.White
+        PictureBox6.BackColor = Color.White
+        PictureBox5.BackColor = Color.White
+        PictureBox7.BackColor = Color.White
+        PictureBox8.BackColor = Color.White
+        PictureBox9.BackColor = Color.Black
+    End Sub
+    Private Sub PictureBox8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox8.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.  
+        If PictureBox9.BackColor = Color.Black Then
+            PictureBox9.Image = PictureBox8.Image
+            PictureBox9.BackColor = Color.White
+            PictureBox8.Image = ImageList1.Images(0)
+            PictureBox8.BackColor = Color.Black
+        End If
+        If PictureBox7.BackColor = Color.Black Then
+            PictureBox7.Image = PictureBox8.Image
+            PictureBox7.BackColor = Color.White
+            PictureBox8.Image = ImageList1.Images(0)
+            PictureBox8.BackColor = Color.Black
+        End If
+        If PictureBox5.BackColor = Color.Black Then
+            PictureBox5.Image = PictureBox8.Image
+            PictureBox5.BackColor = Color.White
+            PictureBox8.Image = ImageList1.Images(0)
+            PictureBox8.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox9.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox6.BackColor = Color.Black Then
+            PictureBox6.Image = PictureBox9.Image
+            PictureBox6.BackColor = Color.White
+            PictureBox9.Image = ImageList1.Images(0)
+            PictureBox9.BackColor = Color.Black
+        End If
+        If PictureBox8.BackColor = Color.Black Then
+            PictureBox8.Image = PictureBox9.Image
+            PictureBox8.BackColor = Color.White
+            PictureBox9.Image = ImageList1.Images(0)
+            PictureBox9.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox7.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox4.BackColor = Color.Black Then
+            PictureBox4.Image = PictureBox7.Image
+            PictureBox4.BackColor = Color.White
+            PictureBox7.Image = ImageList1.Images(0)
+            PictureBox7.BackColor = Color.Black
+        End If
+        If PictureBox8.BackColor = Color.Black Then
+            PictureBox8.Image = PictureBox7.Image
+            PictureBox8.BackColor = Color.White
+            PictureBox7.Image = ImageList1.Images(0)
+            PictureBox7.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox4.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox1.BackColor = Color.Black Then
+            PictureBox1.Image = PictureBox4.Image
+            PictureBox1.BackColor = Color.White
+            PictureBox4.Image = ImageList1.Images(0)
+            PictureBox4.BackColor = Color.Black
+        End If
+        If PictureBox5.BackColor = Color.Black Then
+            PictureBox5.Image = PictureBox4.Image
+            PictureBox5.BackColor = Color.White
+            PictureBox4.Image = ImageList1.Images(0)
+            PictureBox4.BackColor = Color.Black
+        End If
+        If PictureBox7.BackColor = Color.Black Then
+            PictureBox7.Image = PictureBox4.Image
+            PictureBox7.BackColor = Color.White
+            PictureBox4.Image = ImageList1.Images(0)
+            PictureBox4.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox5.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox2.BackColor = Color.Black Then
+            PictureBox2.Image = PictureBox5.Image
+            PictureBox2.BackColor = Color.White
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.Black
+        End If
+        If PictureBox4.BackColor = Color.Black Then
+            PictureBox4.Image = PictureBox5.Image
+            PictureBox4.BackColor = Color.White
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.Black
+        End If
+        If PictureBox8.BackColor = Color.Black Then
+            PictureBox8.Image = PictureBox5.Image
+            PictureBox8.BackColor = Color.White
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.Black
+        End If
+        If PictureBox6.BackColor = Color.Black Then
+            PictureBox6.Image = PictureBox5.Image
+            PictureBox6.BackColor = Color.White
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox6.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox3.BackColor = Color.Black Then
+            PictureBox3.Image = PictureBox6.Image
+            PictureBox3.BackColor = Color.White
+            PictureBox6.Image = ImageList1.Images(0)
+            PictureBox6.BackColor = Color.Black
+        End If
+        If PictureBox9.BackColor = Color.Black Then
+            PictureBox9.Image = PictureBox6.Image
+            PictureBox9.BackColor = Color.White
+            PictureBox6.Image = ImageList1.Images(0)
+            PictureBox6.BackColor = Color.Black
+        End If
+        If PictureBox5.BackColor = Color.Black Then
+            PictureBox5.Image = PictureBox6.Image
+            PictureBox5.BackColor = Color.White
+            PictureBox6.Image = ImageList1.Images(0)
+            PictureBox6.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox2.BackColor = Color.Black Then
+            PictureBox2.Image = PictureBox3.Image
+            PictureBox2.BackColor = Color.White
+            PictureBox3.Image = ImageList1.Images(0)
+            PictureBox3.BackColor = Color.Black
+        End If
+        If PictureBox6.BackColor = Color.Black Then
+            PictureBox6.Image = PictureBox3.Image
+            PictureBox6.BackColor = Color.White
+            PictureBox3.Image = ImageList1.Images(0)
+            PictureBox3.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox2.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox1.BackColor = Color.Black Then
+            PictureBox1.Image = PictureBox2.Image
+            PictureBox1.BackColor = Color.White
+            PictureBox2.Image = ImageList1.Images(0)
+            PictureBox2.BackColor = Color.Black
+        End If
+        If PictureBox3.BackColor = Color.Black Then
+            PictureBox3.Image = PictureBox2.Image
+            PictureBox3.BackColor = Color.White
+            PictureBox2.Image = ImageList1.Images(0)
+            PictureBox2.BackColor = Color.Black
+        End If
+        If PictureBox5.BackColor = Color.Black Then
+            PictureBox5.Image = PictureBox2.Image
+            PictureBox5.BackColor = Color.White
+            PictureBox2.Image = ImageList1.Images(0)
+            PictureBox2.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        'Funcion que se ejecuta al hacer clic en el PictureBox8, los codicionales permiten realizar una comparacion entre los 
+        'colores de fondo de los PictureBox y al cumplir esta condicion es asignado la imagen del antesesor.
+        If PictureBox2.BackColor = Color.Black Then
+            PictureBox2.Image = PictureBox1.Image
+            PictureBox2.BackColor = Color.White
+            PictureBox1.Image = ImageList1.Images(0)
+            PictureBox1.BackColor = Color.Black
+        End If
+        If PictureBox4.BackColor = Color.Black Then
+            PictureBox4.Image = PictureBox1.Image
+            PictureBox4.BackColor = Color.White
+            PictureBox1.Image = ImageList1.Images(0)
+            PictureBox1.BackColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Carga el formulario y presenta las imagenes originales y las que se van a mover asi poder dar inicio al juego.
+        PictureBox1.Image = ImageList1.Images(1)
+        PictureBox2.Image = ImageList1.Images(2)
+        PictureBox3.Image = ImageList1.Images(3)
+        PictureBox4.Image = ImageList1.Images(4)
+        PictureBox6.Image = ImageList1.Images(5)
+        PictureBox5.Image = ImageList1.Images(6)
+        PictureBox7.Image = ImageList1.Images(7)
+        PictureBox8.Image = ImageList1.Images(8)
+        PictureBox9.Image = ImageList1.Images(0)
+    End Sub
+    'Codigo realizado por
+    'JOHNNY RIVEROS
+    'JUAN MARTINEZ
+    'OSCAR TIQUE
+    'FECHA REALIZACION 10-04-2015
+    'VERSION 001
+End Class
